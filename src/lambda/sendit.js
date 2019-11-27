9894736ced391f0d43a028204f446be160085cc7
Cosmin Popovici <hellocosmin@gmail.com>
2019-11-27 15:18:16 +0200

    Temporarily remove Akismet.

--- src/lambda/sendit.js
+++ src/lambda/sendit.js
@@ -1,9 +1,3 @@
-const akismet = require('akismet');
-const akismetClient = akismet.client({
-  apiKey: process.env.AKISMET_API_KEY,
-  blog: 'https://sendtest.email'
-});
-
 const SparkPost = require('sparkpost');
 const sparkpostClient = new SparkPost(process.env.SPARKPOST_API_KEY);
 
@@ -26,15 +20,6 @@ exports.handler = function (event, context, callback) {
 
   const payload = JSON.parse(event.body);
 
-  if (payload.subject == process.env.FU) {
-    return callback(null, {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: 'Resource not found',
-      }),
-    });
-  }
-
   if (
     !payload.subject ||
     !payload.recipient ||
@@ -49,66 +34,40 @@ exports.handler = function (event, context, callback) {
     });
   }
 
-  akismetClient.checkComment({
-    user_ip : event.headers['x-nf-client-connection-ip'], // Required!
-    user_agent : event.headers['user-agent'], // Required!
-    referrer : event.headers['referrer'], // Required!
-    comment_author_email : payload.recipient,
-    comment_content : payload.html,
-  }, function(err, spam) {
-    if (err) {
-      return callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: 'Internal server error',
-        }),
-      });
-    }
-
-    if (spam) {
-      return callback(null, {
-        statusCode: 403,
-        body: JSON.stringify({
-          message: 'Forbidden',
-        }),
-      });
-    }
-
-    sparkpostClient.transmissions.send({
-      options: {
-        open_tracking: false,
-        click_tracking: false
+  sparkpostClient.transmissions.send({
+    options: {
+      open_tracking: false,
+      click_tracking: false
+    },
+    content: {
+      from: {
+        name: 'SendTest.Email',
+        email: 'tests@mail.sendtest.email'
       },
-      content: {
-        from: {
-          name: 'SendTest.Email',
-          email: 'tests@mail.sendtest.email'
-        },
-        subject: payload.subject,
-        html: payload.html,
-        amp_html: payload.amp_html,
-        text: payload.text,
-      },
-      recipients: [
-        {address: payload.recipient}
-      ]
-    })
-    .then(data => {
-      return callback(null, {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({
-          data: data
-        }),
-      });
-    })
-    .catch(err => {
-      return callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          data: err,
-        })
-      });
+      subject: payload.subject,
+      html: payload.html,
+      amp_html: payload.amp_html,
+      text: payload.text,
+    },
+    recipients: [
+      {address: payload.recipient}
+    ]
+  })
+  .then(data => {
+    return callback(null, {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        data: data
+      }),
+    });
+  })
+  .catch(err => {
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        data: err,
+      })
     });
   });
 };
