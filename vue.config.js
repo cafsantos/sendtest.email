7a3f3825e9c879b7470019309b31559ea7805846
Cosmin Popovici <hellocosmin@gmail.com>
2019-12-02 19:13:00 +0200

    Fix Netlify deploy previews.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+let fullURL = process.env.NODE_ENV === 'production' ? 'https://sendtest.email/' : '/'
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://sendtest.email/'
-    : '/'
+  publicPath: process.env.CONTEXT === 'deploy-preview' ? `${process.env.DEPLOY_PRIME_URL}/` : fullURL,
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config
+        .plugin('html')
+        .tap(([args]) => {
+          args.xhtml = true
+          args.minify.removeAttributeQuotes = false
+          return [args]
+        })
+    }
+  }
 }
